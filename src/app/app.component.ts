01d0c07d78837781717e762bc5b4e15d3e20b015
Andreea Ciforac <andreea_ciforac@yahoo.com>
2020-04-23 18:32:50 +0300

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'nasa-natural-event-tracker-platform';
+}
