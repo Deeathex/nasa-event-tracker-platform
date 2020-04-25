96055fe5cb43890fa3a184c888df9b6906b4ae60
Andreea Ciforac <andreea_ciforac@yahoo.com>
2020-04-25 16:06:49 +0300

    Basic classes

---
+++ src/app/models/models.ts
@@ -0,0 +1,39 @@
+export enum EventStatus {
+  open,
+  closed,
+  all
+}
+
+export interface Category {
+  id: string;
+  title: string;
+  description?: string;
+}
+
+export interface Event {
+  id: string;
+  title: string;
+  description?: string;
+  link?: string;
+  categories?: Category[];
+  sources?: Source[];
+  geometries?: Geometry[];
+  closed?: string;
+}
+
+export interface Source {
+  id: string;
+  title?: string;
+  url?: string;
+}
+
+export interface Geometry {
+  id: string;
+  date?: Date;
+  coordinates?: Coordinates;
+}
+
+export interface Coordinates {
+  latitude?: number;
+  longitude?: number;
+}
